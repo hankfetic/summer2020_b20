a1b4b4f89bb6d44f28f8c57ac95bdb64df3e82d7
deception <hankfetic@gmail.com>
2020-08-01 15:46:36 -0500

    yul

---
+++ src/kkkkkkkkk/liol.java
@@ -0,0 +1,7 @@
+package kkkkkkkkk;
+
+public class liol {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
--- src/yyyyyyy/sout.java
+++ src/yyyyyyy/sout.java
@@ -3,5 +3,7 @@ package yyyyyyy;
 public class sout {
     public static void main(String[] args) {
         System.out.println("12");
+
+        System.out.println("trying something new");
     }
 }
